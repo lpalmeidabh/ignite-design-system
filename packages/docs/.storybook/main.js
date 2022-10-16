b9b577d15ed39d69a2f1c38ec5d5f191001b6754
Lucas Almeida <lpalmeidabh@gmail.com>
2022-10-16 18:01:19 -0300

    ci: Add docs workflow

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -15,5 +15,11 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
+  },
+  viteFinal: (config, { configType }) => {
+    if(configType === 'production'){
+      config.base = '/ignite-design-system/'
+    }
+    return config
   }
 }
